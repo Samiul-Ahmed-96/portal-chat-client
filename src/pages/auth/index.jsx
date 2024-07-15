81bb17eb68216959dd6cbba7f84151bb2093379b
Samiul-Ahmed-96 <samiulahmedanik45@gmail.com>
2024-07-15 16:28:04 +0600

    Added auth ui

--- src/pages/auth/index.jsx
+++ src/pages/auth/index.jsx
@@ -1,8 +1,88 @@
+import victory from "@/assets/victory.svg";
+import { Input } from "@/components/ui/input";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { useState } from "react";
 
 const Auth = () => {
-  return (
-    <div>Auth</div>
-  )
-}
 
-export default Auth
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmePassword, setConfirmPassword] = useState("");
+
+  return (
+    <div className="h-[100vh] w-[100vw] flex items-center justify-center">
+      <div className="h-[80vh] bg-white border-2 border-white text-opacity-90 shadow-2xl w-[80vw] md:w-[90vw] lg:w-[70vw] xl:w-[60vw] rounded-3xl grid xl:grid-cols-2">
+        <div className="flex flex-col gap-10 items-center justify-center">
+          <div className="flex items-center justify-center flex-col">
+            <div className="flex items-center justify-center">
+              <h1 className="text-5xl font-bold md:text-6xl text-green-800">Welcome</h1>
+              <img src={victory} alt="victory emoji" className="h-[100px]" />
+            </div>
+            <p className="font-medium text-center">
+              Fill in the details to get started with the best chat app !
+            </p>
+          </div>
+          <div className="flex justify-center items-center w-full">
+            <Tabs className="w-3/4">
+              <TabsList className="bg-transparent rounded-none w-full">
+                <TabsTrigger
+                  value="login"
+                  className="data-[state=active]:bg-transparent text-black text-opacity-90 border-b-2 rounded-none w-full data-[state=active]:text-black data-[state=active]:font-semibold data-[state=active]:border-b-green-800 p-3 transition-all duration-300"
+                >
+                  Login
+                </TabsTrigger>
+                <TabsTrigger
+                  value="signup"
+                  className="data-[state=active]:bg-transparent text-black text-opacity-90 border-b-2 rounded-none w-full data-[state=active]:text-black data-[state=active]:font-semibold data-[state=active]:border-b-green-800 p-3 transition-all duration-300"
+                >
+                  Sign Up
+                </TabsTrigger>
+              </TabsList>
+              <TabsContent className="flex flex-col gap-5 mt-10" value="login">
+                <Input
+                  placeholder="Email"
+                  type="email"
+                  className="rounded-3 p-6"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                ></Input>
+                <Input
+                  placeholder="Password"
+                  type="password"
+                  className="rounded-3 p-6"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                ></Input>
+              </TabsContent>
+              <TabsContent className="flex flex-col gap-5 mt-10" value="signup">
+                <Input
+                  placeholder="Email"
+                  type="email"
+                  className="rounded-3 p-6"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                ></Input>
+                <Input
+                  placeholder="Password"
+                  type="password"
+                  className="rounded-3 p-6"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                ></Input>
+                <Input
+                  placeholder="Confirm Password"
+                  type="password"
+                  className="rounded-3 p-6"
+                  value={confirmePassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                ></Input>
+              </TabsContent>
+            </Tabs>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Auth;

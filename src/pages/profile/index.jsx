3917aa6d2c0224f1e5e3b39c92d31169a87ab2c4
Samiul-Ahmed-96 <samiulahmedanik45@gmail.com>
2024-07-17 20:38:18 +0600

    Create profile ui and and develop update profile functionality

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -1,10 +1,162 @@
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { apiClient } from "@/lib/api-client";
+import { colors, getColor } from "@/lib/utils";
 import { useAppStore } from "@/store";
+import { UPDATE_PROFILE_ROUTE } from "@/utils/constants";
+import { useEffect, useState } from "react";
+import { FaPlus, FaTrash } from "react-icons/fa";
+import { IoArrowBack } from "react-icons/io5";
+import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
 
 const Profile = () => {
-  const {userInfo} = useAppStore();
-  return (
-    <div>Profile Email : {userInfo.email}</div>
-  )
-}
+  const navigate = useNavigate();
+  const { userInfo, setUserInfo } = useAppStore();
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [image, setImage] = useState(null);
+  const [hoverd, setHoverd] = useState(false);
+  const [selectedColor, setSelectedColor] = useState(0);
 
-export default Profile
+  useEffect(()=>{
+    if(userInfo.profileSetup){
+      setFirstName(userInfo.firstName);
+      setLastName(userInfo.lastName)
+      setSelectedColor(userInfo.color)
+    }
+  },[userInfo])
+
+  const validateProfile = () => {
+    if (!firstName) {
+      toast("First Name is required");
+      return false;
+    }
+    if (!lastName) {
+      toast("Last Name is required");
+      return false;
+    }
+    return true;
+  };
+
+  const saveChanges = async () => {
+    if (validateProfile()) {
+      try {
+        const response = await apiClient.post(
+          UPDATE_PROFILE_ROUTE,
+          { firstName, lastName, color: selectedColor },
+          { withCredentials: true }
+        );
+        if (response.status === 200 && response.data) {
+          setUserInfo({ ...response.data });
+          toast.success("Profile updated successfully");
+          navigate('/chat');
+        }
+      } catch (error) {
+        console.log({ error });
+      }
+    }
+  };
+
+  return (
+    <div className="bg-[#1b1c24] h-[100vh] flex items-center justify-center flex-col gap-10">
+      <div className="flex flex-col gap-10 w-[80vw] md:w-max">
+        <div>
+          <IoArrowBack className="text-4xl lg:text-6xl text-white/90 cursor-pointer" />
+        </div>
+        <div className="grid grid-cols-2">
+          <div
+            className="h-full w-32 md:w-48 relative flex items-center justify-center"
+            onMouseEnter={() => setHoverd(true)}
+            onMouseLeave={() => setHoverd(false)}
+          >
+            <Avatar className="h-32 w-32 md:h-48 md:w-48 rounded-full overflow-hidden">
+              {image ? (
+                <AvatarImage
+                  src={image}
+                  alt="profile"
+                  className="object-cover w-full h-full bg-black"
+                />
+              ) : (
+                <div
+                  className={`h-32 w-32 md:h-48 md:w-48 uppercase text-5xl border-[1px] flex justify-center items-center rounded-full ${getColor(
+                    selectedColor
+                  )}`}
+                >
+                  {firstName
+                    ? firstName.split("").shift()
+                    : userInfo.email.split("").shift()}
+                </div>
+              )}
+            </Avatar>
+            {hoverd && (
+              <div className="absolute inset-0 flex justify-center items-center rounded-full bg-black/50 cursor-pointer ring-fuchsia-50">
+                {image ? (
+                  <FaTrash className="text-white text-3xl cursor-pointer" />
+                ) : (
+                  <FaPlus className="text-white text-3xl cursor-pointer" />
+                )}
+              </div>
+            )}
+            {/* <input type="text" name="" id="" /> */}
+          </div>
+          <div className="flex min-w-32 md:min-w-64 flex-col gap-5 text-white items-center justify-center">
+            <div className="w-full">
+              <Input
+                placeholder="Email"
+                type="email"
+                disabled
+                value={userInfo.email}
+                className="rounded-lg p-6 bg-[#2c2e3b] border-none"
+              />
+            </div>
+            <div className="w-full">
+              <Input
+                placeholder="First Name"
+                type="text"
+                value={firstName}
+                className="rounded-lg p-6 bg-[#2c2e3b] border-none"
+                onChange={(e) => setFirstName(e.target.value)}
+              />
+            </div>
+            <div className="w-full">
+              <Input
+                placeholder="Last Name"
+                type="text"
+                value={lastName}
+                className="rounded-lg p-6 bg-[#2c2e3b] border-none"
+                onChange={(e) => setLastName(e.target.value)}
+              />
+            </div>
+            <div className="w-full flex gap-5">
+              {colors.map((color, i) => (
+                <div
+                  onClick={() => setSelectedColor(i)}
+                  key={i}
+                  className={`${color} h-8 w-8 rounded-full cursor-pointer transition-all duration-300 ${
+                    selectedColor === i
+                      ? "outline outline-white/50 outline-2"
+                      : ""
+                  }`}
+                >
+                  <div />
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className="w-full">
+          <Button
+            onClick={saveChanges}
+            className="h-16 w-full bg-green-800 hover:bg-green-900 transition-all duration-300"
+          >
+            Save Changes
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;

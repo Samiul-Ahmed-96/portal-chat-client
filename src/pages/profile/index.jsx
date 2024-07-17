b784b7698b4fdcd1dae641141cf5da6e9f113425
Samiul-Ahmed-96 <samiulahmedanik45@gmail.com>
2024-07-17 20:47:10 +0600

    Added handle navigate

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -59,10 +59,18 @@ const Profile = () => {
     }
   };
 
+  const handleNavigate = () =>{
+    if(userInfo.profileSetup){
+      navigate('/chat')
+    }else{
+      toast.error('Please setup the profile')
+    }
+  }
+
   return (
     <div className="bg-[#1b1c24] h-[100vh] flex items-center justify-center flex-col gap-10">
       <div className="flex flex-col gap-10 w-[80vw] md:w-max">
-        <div>
+        <div onClick={handleNavigate}>
           <IoArrowBack className="text-4xl lg:text-6xl text-white/90 cursor-pointer" />
         </div>
         <div className="grid grid-cols-2">
